adfa833daa00a1357f76bd993f650807cb30ab85
JingYu <745294340@qq.com>
2024-08-11 21:37:55 +0800

    补充抽象类的知识

--- AbstractClass.py
+++ AbstractClass.py
@@ -36,3 +36,52 @@ s.say()
 s.hobby()  # 子类实现抽象父类的抽象方法
 
 
+# 抽象类
+class A:
+    def cool(self):
+        pass
+
+    def hot(self):
+        pass
+
+    def swing(self):
+        pass
+
+
+class Midea(A):
+    def cool(self):
+        print('M_cool')
+
+    def hot(self):
+        print('M_hot')
+
+    def swing(self):
+        print('M_swing')
+
+
+class GREE(A):
+    def cool(self):
+        print('G_c')
+
+    def hot(self):
+        print('G_h')
+
+    def swing(self):
+        print('G_s')
+
+
+def make(a: A):
+    a.hot()
+    a.cool()
+    a.swing()
+
+
+a: A  # 抽象类是不能实例化的 a被注解为A类型 a是A的子类的类型
+a = Midea()
+print('*' * 100)
+a.swing()  # 输出M_swing
+print('*' * 100)
+x = Midea()
+y = GREE()
+make(x)
+make(y)
