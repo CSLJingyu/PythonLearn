408a3605f9b7d1bfabd1fae7d922a3aefab1dc94
JingYu <745294340@qq.com>
2024-08-17 16:54:03 +0800

    订单自动回复简易版本

--- text.py
+++ text.py
@@ -1,87 +1,47 @@
-import string
-import random
 import os
-import time
-from datetime import datetime
+import re
 
 
-# 批量创建文件
-# good = ['apple', 'pile', 'smoke', 'egg']
-# for i in range(3):
-#     random_element = random.choice(good)
-#
-#     user_id = ''.join(random.choices('0123456789abcdef', k=9))
-#     f = open(f'./{i:03}_{random_element}_{user_id}.txt', 'w')
-#     f.write("")
-#     f.close()
+# 模型淘宝自动回复
 
+def answer(question):
+    with open('replay.txt', "r", encoding='utf-8') as file:
+        math = re.search(r"订单", question)
+        if math:
+            answer_txt = file.readline()
+            answer_txt = answer_txt.split('|')
+            answer_txt = answer_txt[1].split()
 
-# 批量创建文件夹
-def mkdir(path, nums):
-    for i in range(nums):
-        os.mkdir(path + '/' + str(i))
+        math = re.search(r"物流", question)
+        if math:
+            # 跳过第一行
+            file.readline()
+            answer_txt = file.readline().split()
+            answer_txt = answer_txt.split('|')
+            answer_txt = answer_txt[1].split()
 
+        math = re.search(r"账户", question)
+        if math:
+            # 跳过第二行
+            file.readline()
+            file.readline()
+            answer_txt = file.readline().split()
+            answer_txt = answer_txt.split('|')
+            answer_txt = answer_txt[1].split()
 
-# 记录用户登入日志并且查看
-# 创建系统,每次登入时,将用户的登录日志写入文件,并且可以查看到用户的登入时间 用户名字
+        math = re.search(r"支付", question)
+        if math:
+            # 跳过第三行
+            file.readline()
+            file.readline()
+            file.readline()
+            answer_txt = file.readline().split()
+            answer_txt = answer_txt.split('|')
+            answer_txt = answer_txt[1].split()
 
-class User:
-
-    def __init__(self, name, password):
-        self.name = name
-        self.password = password
-
-
-def show_info(user):
-    user_name = user.name
-    user_time = user.time
-
-
-def random_name():
-    lenth = random.randint(2, 4)
-    name = ''.join(random.choices(string.ascii_letters, k=lenth))  # 随机生成汉字
-    return name
-
-
-def login_info(user):
-    print('登录成功')
-    with open('log.txt', 'a', encoding='utf-8') as file:
-        s = f'用户名:{user.name}  登入时间{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
-        file.write(s + '\n')
-
-
-# 读日志
-def get_login_information():
-    with open('log.txt', "r", encoding='utf-8') as file:
-        while True:
-            line = file.readline()
-            if line ==  '':
-                break
-            else:
-                print(line)
+        return answer_txt
 
 
 if __name__ == '__main__':
-    lst = []
-    for i in range(3):
-        print('name:', end=" ")
-        name = input()
-        age = random.randint(18, 100)
-        password = "password" + str(i)
-        user = User(name, password)
-        lst.append(user)
-
-    for i in range(3):
-        login_info(lst[i])
-        time.sleep(1)
-
-    get_login_information()
-
-# if __name__ == '__main__':
-#     path = './new_dir'
-#     if not os.path.exists(path):
-#         os.mkdir(path)
-#
-#     print('输入创建的个数:', end=" ")
-#     nums = int(input())
-#     mkdir(path, nums)
+    get_answer = answer("订单")
+    print(''.join(get_answer))
