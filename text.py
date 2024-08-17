d3dab9f483171b77d1de5bd1954fa3600b0df2db
JingYu <745294340@qq.com>
2024-08-17 15:30:11 +0800

    目录批量产生

--- text.py
+++ text.py
@@ -1,19 +1,30 @@
-class S:
-    def __init__(self, id, age):
-        self.id = id
-        self.age = age
-
-    def info(self):
-        print(f"{self.id}, {self.age}")
-        # return 12345
+import string
+import random
+import os
 
 
-stu_list = []
-for i in range(2):
-    user_input = input()
-    user_input = user_input.split("##")
-    s = S(user_input[0], user_input[1])
-    stu_list.append(s)
+# 批量创建文件
+good = ['apple', 'pile', 'smoke', 'egg']
+for i in range(3):
+    random_element = random.choice(good)
 
-for i in range(len(stu_list)):
-    print(stu_list[i].info())
+    user_id = ''.join(random.choices('0123456789abcdef', k=9))
+    f = open(f'./{i:03}_{random_element}_{user_id}.txt', 'w')
+    f.write("")
+    f.close()
+
+
+# 批量创建文件夹
+def mkdir(path, nums):
+    for i in range(nums):
+        os.mkdir(path + '/' + str(i))
+
+
+if __name__ == '__main__':
+    path = './new_dir'
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    print('输入创建的个数:', end=" ")
+    nums = int(input())
+    mkdir(path, nums)
